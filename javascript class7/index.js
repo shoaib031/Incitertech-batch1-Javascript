f5024207cfb108392c07f1168d626a7387b29226
Shoaib Siddiqui <125572377+shoaib031@users.noreply.github.com>
2023-08-02 14:39:51 +0500

    Javascript Class 7
    Javascript Class 7

---
+++ javascript class7/index.js
@@ -0,0 +1,19 @@
+
+let total = 0;
+let tasks = [];
+const showTasks = () => {
+    tasks.forEach(task => {
+        return <li>task.amount</li>
+    })
+}
+const addTask = (name, amount, type) => {
+    tasks.push({
+        id : 1,
+        name,
+        amount,
+        type
+    })
+}
+let currentblance = [
+ 
+]
