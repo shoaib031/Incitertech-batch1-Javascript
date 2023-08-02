0fac4e4840635731251269abd93eebf8d61d8005
Shoaib Siddiqui <125572377+shoaib031@users.noreply.github.com>
2023-08-02 14:37:42 +0500

    Javascript Class 3
    Javascript Class 3

---
+++ javascript class3/app.js
@@ -0,0 +1,29 @@
+// var per = prompt("ENTER YOUR PERCDNTAGE");
+
+// if(per >= 80 && per <= 100){
+//     document.write( "YOU ARE IN 1st DIVISION")
+// }else if(per >=60 && per <= 80 ){
+//     document.write( "YOU ARE IN 2nd DIVISION")
+// }else if(per >=40 && per <= 60 ){
+//     document.write( "YOU ARE IN 3rd DIVISION")
+// }else if(per >=20 && per <= 40 ){
+//     document.write( "YOU ARE FAIL")
+// }else{
+//     document.write("PLEASE WRITE CORRECT PERCENTAGE")
+// }
+
+// var obtainesMarks = prompt("ENTER OBTAINED MARKS");
+// var totalMarks = prompt("ENTER TOTAL MARKS");
+// var Percentage = (obtainesMarks / totalMarks) * 100  
+
+//  if(Percentage >= 80){
+//   document.write("YOUR GRADE IS A+")
+//  }else if(Percentage >= 70){
+//   document.write("YOUR GRADE IS A")
+//  }else if(Percentage >= 60 ){
+//   document.write("YOUR GRADE IS B")
+//  }else if(Percentage >= 40 ){
+//     document.write("YOUR GRADE IS C")
+//  }else{
+//     document.write("YOU ARE FAIL")
+//  }
