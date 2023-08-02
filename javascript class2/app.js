49b917a11e0828094207f1f6f4c051130b93a90f
Shoaib Siddiqui <125572377+shoaib031@users.noreply.github.com>
2023-08-02 14:36:27 +0500

    Javascript Class 2
    Javascript Class 2

---
+++ javascript class2/app.js
@@ -0,0 +1,29 @@
+// var num = 3;
+// var nu/m2 = 2
+// var totalNum = num + num2
+// document.write("addition of " + num + "and " + num2 + "is " + totalNum  ) 
+
+// var num = 5;
+// num1 = num++;
+// var num1 = ++num;
+// document.write(num1)
+// var num2 = 10;
+// num3 = num2--;
+// document.write(num1)
+// document.write("<br>")
+// document.write(num2)
+// document.write("<br>")
+// document.write(num3)
+
+// var name = prompt("enter your name")
+// document.write("welcome to mr " + name)
+// document.write(Number("2" + 1))
+
+// var name = prompt()
+// var name1 = prompt()
+// document.write(Number(2 + 3))
+// document.write("addition of 2 and 3 is " + name + name1)
+// var num = 10;
+// num++
+// ++num
+// document.write(num)
