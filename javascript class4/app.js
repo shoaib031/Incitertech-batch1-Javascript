960d6b4604c57efcdd2f0435c54a538054b6ecbe
Shoaib Siddiqui <125572377+shoaib031@users.noreply.github.com>
2023-08-02 14:38:12 +0500

    Javascript Class 4
    Javascript Class 4

---
+++ javascript class4/app.js
@@ -0,0 +1,71 @@
+
+
+// 5
+// 5 4
+// 5 4 3
+// 5 4 3 2
+// 5 4 3 2 1
+
+
+
+// for (var a = 5; a >= 1; a--) {
+    // for (var ind = 5; ind >= a; ind--) {
+        // document.write(ind)
+    // }
+    // document.write("<br>")
+// }
+// document.write("<br>")
+// for (var a = 5; a >= 1; a--){
+//     for(var ind = 5; ind >= a; ind++){
+// document.write(ind)
+//     }
+//      document.write("<br>")
+// }
+for(var a = 5; a >= 1; a--){
+   for(var b = 1; b <= a; b++){
+    document.write(a)
+}
+document.write("<br>")
+}0
+
+// var randomNumber = Math.random()
+// randomNumber = randomNumber * 6
+// document.write(randomNumber)
+// document.write("<br>")
+
+// document.write(randomNumber.toFixed(2))
+// document.write("<br>")
+
+// document.write(Math.round(randomNumber))
+// document.write("<br>")
+
+// document.write(Math.ceil(randomNumber))
+// document.write("<br>")
+
+// document.write(Math.floor(randomNumber))
+// document.write("<br>")
+
+// var num = Math.round(randomNumber)
+
+// document.write(Math.round(randomNumber )+ "<br>")
+// document.write(Math.ceil(randomNumber)+ "<br>")
+// document.write(Math.floor(randomNumber)+ "<br>")
+
+// if(randomNumber == 1){
+//  document.write("head" + "<br>")
+// }else if(randomNumber == 2){
+//     document.write("Tail" + "<br>")
+// }else{
+//     document.write("invalid number")
+// }
+
+// var weight = prompt("ENTER YOUR WEIGHT")
+// weight = Number(weight)
+// if(weight >= 100 ){
+//     document.write("Do some dieting")
+// }else if(weight >= 80.80 ){
+//     document.write("Over weight")
+// }else{
+//     document.write("You are smart")
+// }
+
