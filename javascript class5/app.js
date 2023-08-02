26ab666a8468987c33517e76700e9a900534b7b0
Shoaib Siddiqui <125572377+shoaib031@users.noreply.github.com>
2023-08-02 14:38:34 +0500

    Javascript Class 5
    Javascript Class 5

---
+++ javascript class5/app.js
@@ -0,0 +1,9 @@
+// function avatar(){
+//     document.getElementById("ugly").src = "avatar3.png"
+// }
+
+// function clickMe(){
+//     document.getElementById('boxe2').className += ' update-box' 
+//     document.getElementById('boxe3').className += ' update-box2'
+//     document.getElementById('boxe4').className += ' update-box3'
+// }
