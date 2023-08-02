339f2b065407431c2f2070523965bf905e075700
Shoaib Siddiqui <125572377+shoaib031@users.noreply.github.com>
2023-08-02 14:41:00 +0500

    Javascript Class 9 Test
    Javascript Class 9 Test

---
+++ Javascript Test Class 9/app.js
@@ -0,0 +1,27 @@
+// var UserInput = prompt("Enter a String");
+// for(var i = 0; UserInput.length - 1; i--){
+// console.log(UserInput[i]);
+// }
+// var object1 = {
+// name : "Shoaib",
+// age : 16,
+// }
+// var object2 = {
+//     name : "Ahmed",
+//     age : 20,
+// }
+// var answer = object1 + object2;
+// console.log(answer)
+// document.write(object1.name);
+// document.write(object1.age);
+// document.write(object2.name);
+// document.write(object2.age);
+
+// var object1 = prompt("enter 1st object");
+// var object2 = prompt("enter 2nd object");
+// var answer = object1 + object2
+// document.write(answer)
+
+for(var i = 1; i <= 5; i++){
+    document.write(i + "<br>")
+}
